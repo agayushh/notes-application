94b60aeb513da2a01b2444acf68ea439be8847de
AayushhGoyal <agayush088@gmail.com>
2025-02-10 14:37:40 +0530

    login box created

--- frontend/notesapp/src/pages/Login.jsx
+++ frontend/notesapp/src/pages/Login.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import Signup from "./Signup";
 
 export default function Login() {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+
+      <div className="flex justify-center items-center mt-[-100px] h-[80vh]">
+        <div className="w-[520px] rounded-xl border p-10 mt-52">
+          <form action="" onSubmit={() => {}}>
+            <h1 className="text-4xl mb-7">Login</h1>
+            <div className="flex flex-col gap-y-5">
+              <input
+                type="email"
+                placeholder="enter your email id"
+                className="border p-2 rounded-lg"
+              />
+              <button
+                type="submit"
+                className="bg-black text-white p-2 rounded-lg hover:bg-slate-600"
+              >
+                Login
+              </button>
+            </div>
+            <p className="text-sm mt-4">
+              Haven&apos;t registered yet?
+              <br />
+              <Link to="/signup" className="font-medium underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
