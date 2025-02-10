52e1cb1f9782b46fa161eca01815b230fb7a3296
AayushhGoyal <agayush088@gmail.com>
2025-02-10 21:44:45 +0530

    validating email

--- frontend/notesapp/src/pages/Login.jsx
+++ frontend/notesapp/src/pages/Login.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../components/PasswordInput";
+import { validateEmail } from "../utils/helper";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Invalid email id");
+    }
+  };
+
   return (
     <div>
       <Navbar />
 
       <div className="flex justify-center items-center mt-[-100px] h-[80vh]">
         <div className="w-[520px] rounded-xl border p-10 mt-52">
-          <form action="" onSubmit={() => {}}>
+          <form action="" onSubmit={handleLogin}>
             <h1 className="text-4xl mb-7">Login</h1>
             <div className="flex flex-col gap-y-5">
               <input
                 type="email"
                 placeholder="enter your email id"
                 className="border p-2 rounded-lg"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
               />
 
-              <PasswordInput />
+              <PasswordInput
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              {error && <p className="text-red-500 text-xs">{error}</p>}
               <button
                 type="submit"
                 className="bg-black text-white p-2 rounded-lg hover:bg-slate-600"
