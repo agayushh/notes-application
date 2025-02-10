81b303897f5fc27fb2b232356a9b1bbb4a07213d
AayushhGoyal <agayush088@gmail.com>
2025-02-10 14:10:18 +0530

    routes added and navbar

--- frontend/notesapp/src/App.jsx
+++ frontend/notesapp/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+)
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className='bg-black'>
-
-      hi
-    </div>
-  )
+  return <div>
+    {routes}
+  </div>;
 }
 
-export default App
+export default App;
