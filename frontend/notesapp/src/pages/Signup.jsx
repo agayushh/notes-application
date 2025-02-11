1f2d0511028c9c05475ee3017aa1eeeb5a92a105
AayushhGoyal <agayush088@gmail.com>
2025-02-11 10:09:09 +0530

    signup page ui created

--- frontend/notesapp/src/pages/Signup.jsx
+++ frontend/notesapp/src/pages/Signup.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import PasswordInput from "../components/PasswordInput";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const[name, setName] = useState("")
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+  const[error, setError] = useState(null)
+
+
+  const handleSignup = (e)=>{
+    e.preventDefault();
+
+  }
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <Navbar />
+
+      <div className="flex justify-center items-center mt-[-100px] h-[80vh]">
+        <div className="w-[520px] rounded-xl border p-10 mt-52">
+          <form action="" onSubmit={handleSignup}>
+            <h1 className="text-4xl mb-7">Login</h1>
+            <div className="flex flex-col gap-y-5">
+              <input
+                type="text"
+                placeholder="enter your name"
+                className="border p-2 rounded-lg"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <input
+                type="email"
+                placeholder="enter your email id"
+                className="border p-2 rounded-lg"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+
+              <PasswordInput
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              {error && <p className="text-red-500 text-xs">{error}</p>}
+              <button
+                type="submit"
+                className="bg-black text-white p-2 rounded-lg hover:bg-slate-600"
+              >
+                Login
+              </button>
+            </div>
+            <p className="text-sm mt-4">
+              Haven&apos;t registered yet?
+              <br />
+              <Link to="/signup" className="font-medium underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
