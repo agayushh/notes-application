4b5ef71d75ff1470b40eb9c211b19dca53d3f7ac
AayushhGoyal <agayush088@gmail.com>
2025-02-11 10:58:28 +0530

    getting initials of name for navbar

--- frontend/notesapp/src/components/ProfileInfo.jsx
+++ frontend/notesapp/src/components/ProfileInfo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { getInitials } from "../utils/helper";
 
 export default function ProfileInfo({ logout }) {
   return (
     <div className="flex items-center gap-3">
-      <div className="rounded-full w-16 h-16 bg-slate-300 text-center items-center pt-2">
-        John William
+      <div className="rounded-full w-12 h-12 bg-slate-300 text-center items-center pt-2.5 text-xl">
+        {getInitials("John William")}
       </div>
       <div>
         <p className="font-semibold">John William</p>
