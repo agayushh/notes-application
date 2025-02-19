f7e86a93d7588ca6924dce69e10d3c7dabb94ccc
AayushhGoyal <agayush088@gmail.com>
2025-02-19 09:27:50 +0530

    formatted app.jsx

--- frontend/notesapp/src/App.jsx
+++ frontend/notesapp/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
-
 const routes = (
   <Router>
     <Routes>
@@ -13,16 +12,10 @@ const routes = (
       <Route path="/login" element={<Login />} />
     </Routes>
   </Router>
-)
-
+);
 
 function App() {
-
-
-  return 
-  <div>
-    {routes}
-  </div>;
+  return <div>{routes}</div>;
 }
 
 export default App;
